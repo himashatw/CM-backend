9b175d309fd2cdd7a0f496b58477f13a66ff1086
himashatw <himashatw@gmail.com>
2021-05-23 23:00:13 +0530

    folder structure & mongoDB config added

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("running")
+const express = require('express')
+const mongoose = require('mongoose')
+const { MONGOURI } = require('./keys')
+const app = express()
+
+mongoose.connect(MONGOURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+mongoose.connection.on('connected', () => {
+    console.log("connected to the mongoDB");
+})
+
+mongoose.connection.on('error', () => {
+    console.log("error occured! check mongoDB config");
+})
+
+const PORT = 8080
+app.listen(PORT, () => {
+    console.log("Server is running on port : " + PORT);
+})
---
+++ keys.js
@@ -0,0 +1,5 @@
+const key = {
+    MONGOURI: "mongodb+srv://cmuser:e1kqnuyPrzm2ztAC@cluster0.bbcid.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+}
+
+module.exports = key
