55352d6097ccbd889125ef61a37691a2ed419327
ushan1997 <ushananuka5@gmail.com>
2021-05-30 18:18:39 +0530

    new version added

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express')
+bodyParser = require('body-parser');
 const mongoose = require('mongoose')
+const router = require('./routes/pdfRoute')
 const { MONGOURI } = require('./keys')
+
+
 const app = express()
+
+app.use(express.json());
+
+//app.use(router);
+
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect(MONGOURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
+
+
 
 mongoose.connection.on('connected', () => {
     console.log("connected to the mongoDB");
@@ -16,7 +29,7 @@ mongoose.connection.on('error', () => {
     console.log("error occured! check mongoDB config");
 })
 
-const PORT = 8080
+const PORT = 3090
 app.listen(PORT, () => {
     console.log("Server is running on port : " + PORT);
 })
