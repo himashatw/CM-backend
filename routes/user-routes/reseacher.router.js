1b015ca395a069442ac29a1b0f4df02583eb0236
abimanu-devl <abishekmanusha@gmail.com>
2021-06-29 18:23:31 +0530

    added login to user routes

--- routes/user-routes/reseacher.router.js
+++ routes/user-routes/reseacher.router.js
@@ -4,14 +4,14 @@ const Researcher = require('../../models/users/researcher.js');
 const fileValidation = require('../../middlewares/file-upload/validation.js');
 const multer = require('multer');
 
-const fileFilter = (req,res,cb)=>{
-    cb(null,true)
+const fileFilter = (req, res, cb) => {
+    cb(null, true)
 }
 const upload = multer({
-    fileFilter:fileFilter
+    fileFilter: fileFilter
 })
 
-router.post('/add',upload.single('uploads'),fileValidation,async(req,res)=>{
+router.post('/add', upload.single('uploads'), fileValidation, async (req, res) => {
 
     const fullName = req.body.fullName
     const email = req.body.email
@@ -20,36 +20,60 @@ router.post('/add',upload.single('uploads'),fileValidation,async(req,res)=>{
     const uploads = req.file.buffer
 
     const reseacher = new Researcher({
-        fullName : fullName,
-        email :  email,
-        password : password,
-        phoneNo : phoneNo,
-        uploads : uploads
+        fullName: fullName,
+        email: email,
+        password: password,
+        phoneNo: phoneNo,
+        uploads: uploads
     });
 
-    await reseacher.save((err,reseacher)=>{
-        if(err){
+    await reseacher.save((err, reseacher) => {
+        if (err) {
             return res.status(400).send({
-                errors:err.message
+                errors: err.message
             });
         }
 
         return res.status(201).send({
-            message:'Registered successfully',
+            message: 'Registered successfully',
             reseacher
         })
-         
+
     });
-    
+
 });
 
-router.get('/all',async(req,res)=>{
+router.get('/all', async (req, res) => {
     await Researcher.find({})
-     .then(reseachers =>{
-         res.status(200).send({data:reseachers})
-     }).catch(error =>{
-         res.status(400).send({error:error.message})
-     })
+        .then(reseachers => {
+            res.status(200).send({ data: reseachers })
+        }).catch(error => {
+            res.status(400).send({ error: error.message })
+        })
+})
+
+router.post('/login', async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    await Researcher.findOne({ email: email, password: password }, (err, researcher) => {
+        if (err) {
+            console.log(err)
+            return res.status(500).send({
+                errors: err.message
+            });
+        }
+
+        if (!researcher) {
+            return res.status(404).send({
+                message: 'email or password is mismatch!',
+            });
+        }
+
+        return res.status(200).send({
+            message: 'Login successfully'
+        })
+    })
 })
 
 module.exports = router;
