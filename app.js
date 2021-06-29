e06f7b696a0b0518823b636641e46c79eebe2f83
Himasha Weerasinghe <himashatw@gmail.com>
2021-06-29 11:06:09 +0530

    news routes added

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
-const express = require('express')
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const DBConnection = require('./db/dbConnection.js');
+const DBConnection = require("./db/dbConnection.js");
 
-const researcherRoute = require('./routes/user-routes/reseacher.router.js')
-const attendeeRoute = require('./routes/user-routes/attendee.router.js')
-const workshopconductorRoute = require('./routes/user-routes/workshopConductor.router')
-const reviewerRoute = require('./routes/reviewer-routes/reviewer.router.js')
+const researcherRoute = require("./routes/user-routes/reseacher.router.js");
+const attendeeRoute = require("./routes/user-routes/attendee.router.js");
+const workshopconductorRoute = require("./routes/user-routes/workshopConductor.router");
+const reviewerRoute = require("./routes/reviewer-routes/reviewer.router.js");
+require("./models/News/News");
+require("./models/News/ApprovedNews");
 
 // Connect Database
 DBConnection;
 
-
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/attendee", attendeeRoute);
+app.use("/api/reseacher", researcherRoute);
+app.use("/api/workshopcon", workshopconductorRoute);
+app.use("/api/reviewer", reviewerRoute);
 
-app.use('/api/attendee',attendeeRoute);
-app.use('/api/reseacher',researcherRoute);
-app.use('/api/workshopcon',workshopconductorRoute);
-app.use('/api/reviewer',reviewerRoute);
+//admin routes
+app.use("/api", require("./routes/adminRoutes/news"));
 
-const PORT = 8080
+const PORT = 8080;
 
 app.listen(PORT, () => {
-    console.log("Server is running on port : " + PORT);
-})
+  console.log("Server is running on port : " + PORT);
+});
