b74e4ffa9e9dba0fdbed822870d822ffb142b361
Himasha Weerasinghe <himashatw@gmail.com>
2021-06-30 15:24:24 +0530

    speaker route added for admin

--- routes/adminRoutes/keynotespeakers.js
+++ routes/adminRoutes/keynotespeakers.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const Speakers = mongoose.model("Speakers");
+const Speaker = mongoose.model("Speaker");
 //get all keynotespeakers
 router.get("/keynotespeakers", async (req, res) => {
-  // await
+  await Speaker.find({})
+    .then((result) => {
+      res.status(200).json({ result });
+    })
+    .catch((error) => {
+      res.status(422).json({ error });
+    });
 });
 
 module.exports = router;
