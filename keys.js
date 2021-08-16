1f226858d36dc617efc42c27ab53d41a45048867
Himasha Weerasinghe <68691192+himashatw@users.noreply.github.com>
2021-08-17 00:16:33 +0530

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,5 +1,5 @@
 const key = {
-    MONGOURI: "mongodb+srv://cmuser:e1kqnuyPrzm2ztAC@cluster0.bbcid.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    MONGOURI: "",
     JWT_SECRET: "gfhhf#45hhfb@ghybb**s"
 }
 
